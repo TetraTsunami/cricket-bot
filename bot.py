8dcafb85f2be728a1c1474e7e97d8676e30e515b
Gwen <akamitsunami@gmail.com>
2021-08-24 22:57:11 -0500

    We respect pronouns now, and recognize more variations of "dead chat"

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 import logging
-
 import discord
 import re
 import time
@@ -41,12 +40,22 @@ async def on_message(message):
             time.sleep(0.5)
             await message.channel.send('yes, that\'s a threat')
 
-    if re.search('(?i)(^de.*d chat*)|(^chat de.*d*)', message.content):
-        await message.channel.send('https://cdn.discordapp.com/attachments/534782089846063124/879143198197448764/objection-716514-2.mp4')
-        time.sleep(0.5)
-        await message.channel.send('knock it off, funnyman')
+    if re.search('(?i)(^d.?e.*d.?chat*|^chat d.?e.*d*|^d.e.a?.?d..?c.h.a.t*)', message.content):
+        if (message.guild):
+            await message.channel.send('https://cdn.discordapp.com/attachments/534782089846063124/879143198197448764/objection-716514-2.mp4')
+            time.sleep(0.5)
+            # print(message.author.roles)
+            # we want to respect pronouns, so if someone has a role implying otherwise, we'll try not to call them a man.
+            result = []
+            for role in message.author.roles:
+                if re.search('(?i).*She.*|.*They.*', role.name):
+                    result.append(role.name)
+            if result:
+                await message.channel.send('knock it off, clown')
+            else:
+                await message.channel.send('knock it off, funnyman')
 
-    if re.search('(?i)*cricket*', message.content):
+    if re.search('(?i).*cricket.*', message.content):
         await message.channel.send('i\'m literally right here wow')
 
 
