417ba213a9c41d2e79af3fd73f7af7a77e9356e1
Gwen <akamitsunami@gmail.com>
2021-08-30 10:59:17 -0500

    Move /minecraft into its own cog

--- bot.py
+++ bot.py
@@ -5,13 +5,9 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_option
-import re
 import sys
-import socket
-import io
-import base64
 import json
-from mcstatus import MinecraftServer
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,7 +17,7 @@ cogs_dir = "cogs"
 bot = commands.Bot("c!")
 intents = discord.Intents(messages=True, guilds=True)
 if DEBUG:
-    slash = SlashCommand(bot, sync_commands=True, debug_guild=DEBUG)
+    slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True, debug_guild=DEBUG, override_type = True)
 else:
     slash = SlashCommand(bot, sync_commands=True)
 
@@ -29,121 +25,53 @@ else:
 @bot.event
 async def on_ready():
     print(f"*hacker voice* I\'m in. Started up as {bot.user}")
-    guilds = bot.guilds
-    data = {}
-    for guild in guilds:
-        data[guild.id] = []
-        for channel in guild.channels:
-            data[guild.id].append(channel.id)
-    with open("result.json", "w") as file:
-        json.dump(data, file, indent=4)
 
 @bot.command()
-async def load(extension_name : str):
+async def load(ctx, extension_name : str):
     """Loads an extension."""
     try:
         bot.load_extension(extension_name)
     except (AttributeError, ImportError) as e:
         await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
         return
-    await bot.say("{} loaded.".format(extension_name))
+    await ctx.send("{} loaded.".format(extension_name))
 
 @bot.command()
-async def unload(extension_name : str):
+async def unload(ctx, extension_name : str):
     """Unloads an extension."""
     bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
+    await ctx.send("{} unloaded.".format(extension_name))
     
 
 if __name__ == "__main__":
     for extension in [f.replace('.py', '') for f in os.listdir(cogs_dir) if os.path.isfile(os.path.join(cogs_dir, f))]:
         try:
             bot.load_extension(cogs_dir + "." + extension)
-            print(f'{extension} obtained!')
+            print(f'{extension} loaded!')
         except Exception as e:
             print(f'Failed to get ahold of {extension}.')
             print(sys.exc_info())
 
 
-@slash.subcommand(base="bot",
-                  name="ping",
-                  description="Returns info about the bot's ping")
+@slash.subcommand(base="bot", name="ping", description="Returns info about the bot's ping")
 async def bot_ping(ctx):
     await ctx.send(f"Pong! {round(bot.latency * 1000)} ms.")
 
-
-@slash.subcommand(base="minecraft",
-                  name="ping",
-                  description="Returns info about a server",
-                  options=[
-                      create_option(
-                          name="server",
-                          description="Address of the server",
-                          option_type=3,
-                          required=True
-                      ),
-                      create_option(
-                          name="plugins",
-                          description="Try to list the server's plugins",
-                          option_type=5,
-                          required=False
-                      ),
-                      create_option(
-                          name="persistent",
-                          description="Frequently update this message with new data",
-                          option_type=5,
-                          required=False
-                      )
-                  ])
-async def server_ping(ctx, server, plugins=False, persistent=False):
-    await ctx.defer()
-    try:
-        target = MinecraftServer.lookup(server)
-        status = target.status()
-
-        embed = discord.Embed(title=server, color=0xc84268)
-        embed.add_field(name="Version", value=status.version.name)
-        embed.add_field(
-            name="Players", value=f"{status.players.online}/{status.players.max}")
-        embed.add_field(name="Ping", value=f"{round(status.latency)} ms")
-        try:
-            description = re.sub(r'^\s+|§.', '', status.description)
-            description = re.sub(r'\n\s*', r'\n', description)
-            embed.add_field(name="Description",
-                            value=f"```{description}```", inline=False)
-        except:
-            pass
-        try:
-            query = target.query()
-            if plugins:
-                embed.add_field(name="Online", value='\n'.join(query.players.names))
-                embed.add_field(name="Plugins", value='\n'.join(query.software.plugins))
-            else:
-                embed.add_field(name="Online", value=', '.join(query.players.names), inline=False)
-        except:
-            pass
-        # thumbnails are hard
-        try:
-            data = base64.b64decode(
-                status.favicon.split(',', 1)[-1])
-            file = discord.File(fp=io.BytesIO(data),filename="server_favicon.png")
-            embed.set_thumbnail(url="attachment://server_favicon.png")
-            await ctx.send(file=file, embed=embed)
-        except:
-            print(sys.exc_info())
-            await ctx.send(embed=embed)
-    except socket.timeout:
-        embed = discord.Embed(
-            title=server, description="timed out :(")
-        await ctx.send(embed=embed)
-        print(sys.exc_info())
-
-    except:
-        embed = discord.Embed(
-            title=server, description="halp am havin a stronk")
-        await ctx.send(embed=embed)
-        print(sys.exc_info())
-
+# @slash.subcommand(base="bot", name="reload", description="Reloads a backend module", 
+#                   options=[
+#                     create_option(
+#                           name="module",
+#                           description="Name of the cog",
+#                           option_type=3,
+#                           required=True
+#                       )])
+# async def bot_reload(ctx, module):
+#     try:
+#         bot.reload_extension(module)
+#     except commands.ExtensionError as e:
+#         await ctx.send(f'{e.__class__.__name__}: {e}', hidden = True)
+#     else:
+#         await ctx.send(f"Reloaded {module}!", hidden = True)
 
 logging.basicConfig(level=logging.WARNING)
 bot.run(TOKEN)
---
+++ cogs/Minecraft.py
@@ -0,0 +1,95 @@
+import discord
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option
+
+from mcstatus import MinecraftServer
+
+import re
+import sys
+import socket
+import io
+import base64
+
+class Minecraft(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @cog_ext.cog_subcommand(base="minecraft",
+            name="ping",
+            description="Returns info about a server",
+            options=[
+                create_option(
+                    name="server",
+                    description="Address of the server",
+                    option_type=3,
+                    required=True
+                ),
+                create_option(
+                    name="hidden",
+                    description="Only shows results to you",
+                    option_type=5,
+                    required=False
+                ),
+                create_option(
+                    name="plugins",
+                    description="Try to list the server's plugins",
+                    option_type=5,
+                    required=False
+                ),
+                create_option(
+                    name="persistent",
+                    description="(Not working yet) Frequently update this message with new data",
+                    option_type=5,
+                    required=False
+                )
+                ])
+    async def minecraft_ping(self, ctx: SlashContext, server, hidden=False, plugins=False, persistent=False):
+        await ctx.defer()
+        try:
+            target = MinecraftServer.lookup(server)
+            status = target.status()
+            embed = discord.Embed(title=server, color=0xc84268)
+            embed.add_field(name="Version", value=status.version.name)
+            embed.add_field(
+                name="Players", value=f"{status.players.online}/{status.players.max}")
+            embed.add_field(name="Ping", value=f"{round(status.latency)} ms")
+            try:
+                description = re.sub(r'^\s+|§.', '', status.description)
+                description = re.sub(r'\n\s*', r'\n', description)
+                embed.add_field(name="Description",
+                                value=f"```{description}```", inline=False)
+            except:
+                pass
+            try:
+                query = target.query()
+                if plugins:
+                    embed.add_field(name="Online", value='\n'.join(query.players.names))
+                    embed.add_field(name="Plugins", value='\n'.join(query.software.plugins))
+                else:
+                    embed.add_field(name="Online", value=', '.join(query.players.names), inline=False)
+            except:
+                pass
+            # thumbnails are hard
+            try:
+                data = base64.b64decode(
+                    status.favicon.split(',', 1)[-1])
+                file = discord.File(fp=io.BytesIO(data),filename="server_favicon.png")
+                embed.set_thumbnail(url="attachment://server_favicon.png")
+                await ctx.send(file=file, embed=embed,hidden=hidden)
+            except:
+                print(sys.exc_info())
+                await ctx.send(embed=embed,hidden=hidden)
+        except socket.timeout:
+            embed = discord.Embed(
+                title=server, description="timed out :(")
+            await ctx.send(embed=embed,hidden=hidden)
+            print(sys.exc_info())
+        except:
+            embed = discord.Embed(
+                title=server, description="halp am havin a stronk")
+            await ctx.send(embed=embed,hidden=hidden)
+            print(sys.exc_info())
+        
+def setup(bot):
+    bot.add_cog(Minecraft(bot))
