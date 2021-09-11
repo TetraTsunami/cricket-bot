8a4e7b42cc33bca4b1f02f75a045f44fb10eecc3
Gwen <akamitsunami@gmail.com>
2021-09-11 11:21:12 -0500

    fix the resolution of /minecraft say

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -115,7 +115,7 @@ def text_on_img(fp="image_gen.png", text="Hello", size=12, color=(255,255,0)):
 	"Draw a text on an Image, saves it, show it"
 	fnt = ImageFont.truetype('Minecraftia-Regular.ttf', size)
 	# create image
-	image = Image.new(mode = "RGB", size = (int(size/1.5)*len(text),size+50), color = "black")
+	image = Image.new(mode = "RGB", size = (int(size/1.4)*len(text),size+30), color = "black")
 	draw = ImageDraw.Draw(image)
 	# draw text
 	draw.text((10,10), text, font=fnt, fill=color)
