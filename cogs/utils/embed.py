d7816ec37e39ff3569dbc317796c0246879973c6
Gwen <akamitsunami@gmail.com>
2021-12-25 10:49:31 -0600

    add more keys to embed

--- cogs/utils/embed.py
+++ cogs/utils/embed.py
@@ -9,13 +9,15 @@ def simple_embed(title,icon='None',status='None'):
         'YouTube':'<:youtubeicon:884155771556859984>',
         'Twitch':'<:twitchicon:887868334979317801>',
         'Discord':'<:discordicon:887868333716807680>',
-        'Smash':'<:smashicon:895839672956239882>'
+        'Smash':'<:smashicon:895839672956239882>',
+        'Minecraft':'<:GrassBlock:924075881562009640>'
               }
     emoji = EMOJI_KEY[icon]
     STATUS_KEY = {
         'None':'',
         'idk':'❌ Something broke somewhere, try again later',
-        'Permissions':'❌ I don\'t have permission to do that :('
+        'Permissions':'❌ I don\'t have permission to do that :(',
+        'Length':'❌Please don\'t paste the entire Bee Movie script, it\'s much too long'
               }
     if status in STATUS_KEY:
         description = STATUS_KEY[status]
