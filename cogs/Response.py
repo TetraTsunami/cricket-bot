88ef168aa36e421c9e6c0cad1fe4b2cf316e03c7
Gwen <akamitsunami@gmail.com>
2021-12-28 11:08:11 -0600

    this is just unfortunate at this point :(

--- cogs/Response.py
+++ cogs/Response.py
@@ -64,9 +64,9 @@ class Fun(commands.Cog):
 
         if re.search('(?i)^wah\?$', message.content):
             async with message.channel.typing():
-                await message.author.avatar.save("avatar_image_gen.png")
-                im1 = Image.open('avatar_image_gen.png').resize((115,115))
-                im2 = Image.open('wah_outfit.png')
+                await message.author.avatar.save("./image_gen/avatar_image_gen.png")
+                im1 = Image.open('./image_gen/avatar_image_gen.png').resize((115,115))
+                im2 = Image.open('./image_gen/wah_outfit.png')
                 image = Image.new("RGBA", (280,367), (255, 255, 255, 0))
                 image.paste(im1,(82,53))
                 image.alpha_composite(im2)
@@ -75,7 +75,7 @@ class Fun(commands.Cog):
                 await message.channel.send(file=file)
             return
         
-        if (re.search('(?i)^wah.?$', message.content) and message.author.id == 361660269694287883):
+        if ((re.search('(?i)^wah.?$', message.content) and message.author.id == 361660269694287883) or re.search('(?i)^wah\!\?$', message.content)):
             async with message.channel.typing():
                 image = Image.open("./image_gen/wah.png")
                 angle=random.randrange(0,360,1)
