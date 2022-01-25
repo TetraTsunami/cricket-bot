87db8d1d22b88f67f1eb9f999b40b3f8111406e8
Gwen <akamitsunami@gmail.com>
2022-01-25 11:38:34 -0600

    Let's try to add documentation!

--- cogs/utils/deepmoji.py
+++ cogs/utils/deepmoji.py
@@ -1,16 +1,25 @@
 
-# from time import time
 import requests
 import re
 
-def get_emoji(query: str, url: str, min_prob: int = 0.05, results: int = 5):
+def get_emoji(query: str, url: str, min_prob: int = 0.05, results: int = 5) -> list:
+    """
+    Get the top emoji for qiven string using a Deepmoji API instance
+    
+    Args
+        query (str): The string you would like to send to the API.
+        url (str): Link to an instance of https://github.com/nolis-llc/DeepMoji-docker.
+        min_prob (int): Filter results by probability (ex. if min_prob is 0.05, no results with a probability below 0.05 will be returned)
+        results (int): The maximum number of results to return.
+    Returns:
+        A list of dictionaries with keys "emoji" and "prob" from the API
+    Raises:
+        HTTPError: if the API cannot be reached or returns an invalid response.
+    """
     x = re.sub("[<>:'\"]|\d{5,}","",query)
-    # start = time()
-    # print(f'Sending {x} to Deepmoji API')
     data = {"sentences":[(x)]}
     payload = str(data).replace("'","\"")
     r = requests.post(url, data=payload.encode('utf-8'))
-    # print(f'Got API response after {round(time()-start,2)}s')
     r.raise_for_status()
     response = r.json()['emoji'][0]
     def get_prob(e):
