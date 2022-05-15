38b184eac5f12f7b58da96a03f4f875cd5bb5241
Gwen <akamitsunami@gmail.com>
2022-05-14 19:14:51 -0500

    Add better documentation

--- cogs/utils/image.py
+++ cogs/utils/image.py
@@ -1,14 +1,29 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def text_on_img(fp="./image_gen/image_gen.png", text="Hello", size=12, color=(255,255,0), font='Minecraftia-Regular.ttf'):
-	"Draw a text on an Image, saves it, show it"
-	fnt = ImageFont.truetype(font, size)
-	image = Image.new(mode = "RGB", size = (int(size/1.3)*len(text),size+30), color = "black")
-	draw = ImageDraw.Draw(image)
-	draw.text((10,10), text, font=fnt, fill=color)
-	image.save(fp)
+
+def text_to_png(text: str, fp="./image_gen/image_gen.png", size=12, color=(255,255,0), font='/image_gen/Minecraftia-Regular.ttf'):
+    """Writes text to an image
+
+    Args:
+        text (str): The text to write.
+        fp (str, optional): The path of the file to save to. Defaults to "./image_gen/image_gen.png".
+        size (int, optional): Fontsize. Defaults to 12.
+        color (tuple, optional): Color of the text. Defaults to (255,255,0).
+        font (str, optional): Path to the font to use. Defaults to '/image_gen/Minecraftia-Regular.ttf'.
+    """
+    
+    fnt = ImageFont.truetype(font, size)
+    image = Image.new(mode = "RGB", size = (int(size/1.3)*len(text),size+30), color = "black")
+    draw = ImageDraw.Draw(image)
+    draw.text((10,10), text, font=fnt, fill=color)
+    image.save(fp)
  
 def transparency(fp="./image_gen/image_gen.png"):
+    """Turns an image into a transparent image
+
+    Args:
+        fp (str, optional): Path to the image to transparentify. Defaults to "./image_gen/image_gen.png".
+    """
     img = Image.open(fp)
     rgba = img.convert("RGBA")
     data = rgba.getdata()
