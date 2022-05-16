bedd8914141f9029e244061aac88e68f30785247
Gwen <akamitsunami@gmail.com>
2022-05-15 19:39:43 -0500

    Organize and update perms, embed footers

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+
 import discord
-from discord import message
-from discord.ext import commands
-from discord.commands import slash_command, permissions, Option, SlashCommandGroup
 import owo
+from discord import message
+from discord.commands import Option, SlashCommandGroup, slash_command
+from discord.ext import commands
 
 from .utils.embed import simple_embed
 
@@ -15,7 +16,7 @@ class Utilities(commands.Cog):
         self.bot = bot
     
     @slash_command(description="Make me say something like the grim puppetmaster you wish you could be.", default_permission=False) 
-    @permissions.is_owner()
+    @discord.ext.commands.is_owner()
     async def say(self, ctx, text: Option(str, 'What you\'d like to say')):
         await ctx.channel.send(content=text)
         await ctx.respond("<:photothumbsup:886416067021381663><:Yeah:870075068644999248>",ephemeral=True)
@@ -23,30 +24,30 @@ class Utilities(commands.Cog):
     @slash_command(description="Owoify your text!") 
     async def owoify(self, ctx, text: Option(str, 'What you\'d like to say')):
         owotext = owo.owo(text)
-        await ctx.respond(embed=simple_embed('Owoify', 'None', owotext, ctx, 'owoify'), ephemeral=True)
+        await ctx.respond(embed=simple_embed('Owoify', '', owotext, ctx=ctx), ephemeral=True)
     
     snipe = SlashCommandGroup("snipe", "View a message before it was deleted or edited")
         
     @snipe.command(description="View recently deleted messages", default_permission=False) 
-    @permissions.is_owner()
+    @discord.ext.commands.is_owner()
     async def deleted(self, ctx):
         if ctx.guild.id not in deleted_messages:
             await ctx.respond(embed=simple_embed('View Deleted Messages','Discord','NotFound'),ephemeral=True)
             return
         cache = deleted_messages[ctx.guild.id] 
         page_contents = '\n\n'.join(f'{discord.utils.format_dt(time, style="R")} in {message.channel.mention} from {message.author.mention}:\n{message.clean_content}' for time, message in cache.items())
-        await ctx.respond(embed=simple_embed('View Deleted Messages','Discord',page_contents),ephemeral=True)
+        await ctx.respond(embed=simple_embed('View Deleted Messages','Discord',page_contents, ctx=ctx),ephemeral=True)
         
     @snipe.command(description="View recently edited messages", default_permission=False) 
-    @permissions.is_owner()
+    @discord.ext.commands.is_owner()
     async def edits(self, ctx):
         if ctx.guild.id not in edited_messages['after']:
-            await ctx.respond(embed=simple_embed('View Edited Messages','Discord','NotFound'),ephemeral=True)
+            await ctx.respond(embed=simple_embed('View Edited Messages','Discord','NotFound', ctx=ctx),ephemeral=True)
             return
         cache_before = edited_messages['before'][ctx.guild.id]
         cache_after = edited_messages['after'][ctx.guild.id] 
         page_contents = '\n\n'.join(f'{discord.utils.format_dt(time, style="R")} in {message.channel.mention} by {message.author.mention}:\n**FROM:** {cache_before[idx].content}\n**TO:** {message.clean_content}' for idx, (time, message) in enumerate(cache_after.items()))
-        await ctx.respond(embed=simple_embed('View Edited Messages','Discord',page_contents),ephemeral=True)
+        await ctx.respond(embed=simple_embed('View Edited Messages','Discord',page_contents, ctx=ctx),ephemeral=True)
         
     @commands.Cog.listener()
     async def on_message_delete(self, message):
