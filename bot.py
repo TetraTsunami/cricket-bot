bb7e8dfc8b3ec21b484236d4c23979efd0225210
Gwen <akamitsunami@gmail.com>
2022-05-31 11:43:17 -0500

    move reloading to admin cog

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import sys
 
 import discord
 from discord.commands import Option, SlashCommandGroup
@@ -11,10 +10,15 @@ from dotenv import load_dotenv
 from cogs.utils.embed import simple_embed
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+
+loggingConsole = logging.StreamHandler()
+loggingConsole.setLevel(logging.INFO)
+logger.addHandler(loggingConsole)
+
+loggingFile = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+loggingFile.setLevel(logging.DEBUG)
+loggingFile.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(loggingFile)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -29,17 +33,15 @@ intents.messages = True
 intents.guilds = True
 intents.guild_messages = True
 
-if os.getenv('DEBUG_GUILD'): bot = commands.Bot(command_prefix="c!", activity=activity, debug_guilds=[DEBUG], intents=intents)
-else: bot = commands.Bot(command_prefix="c!", activity=activity, intents=intents)
-discord.http.API_VERSION = 9
+bot = commands.Bot(command_prefix="c!", activity=activity, intents=intents)
+if os.getenv('DEBUG_GUILD'): bot.debug_guilds=[DEBUG]
 
 
 @bot.event
 async def on_ready():
-    print(f"\r✓ Logged in as {bot.user} ({bot.user.id})", end = "\n")
-    if os.getenv('DEBUG_GUILD'):
-        debug_guild = await bot.fetch_guild(DEBUG)
-        print(f"✓ Debug guild is {debug_guild.name} ({DEBUG})")
+    logger.info(f"\r✓ Logged in as {bot.user} ({bot.user.id})")
+    if bot.debug_guilds: print(f"✓ Debug guild ID is {DEBUG}")
+        
 
 def list_cogs():
     return [extension for extension in [f.replace('.py', '') for f in os.listdir(cogs_dir) if os.path.isfile(os.path.join(cogs_dir, f))]]
@@ -47,68 +49,15 @@ def list_cogs():
 if __name__ == "__main__":
     progress = 0
     total = len(list_cogs())
-    for extension in [f.replace('.py', '') for f in os.listdir(cogs_dir) if os.path.isfile(os.path.join(cogs_dir, f))]:
+    for extension in list_cogs():
         progress = progress + 1
         try:
             bot.load_extension(cogs_dir + "." + extension)
-            print(f'{progress}/{total} {extension} loaded!')
+            print(f'{progress}/{total} {extension} loaded')
         except Exception as e:
-            print(f'{progress}/{total} {extension} broke!')
-            print(f"→ {sys.exc_info()}")
-        except KeyboardInterrupt:
-            print('\ncya!')
-            quit()
-    print('\r… Modules loaded, logging in to Discord', end = "\r")
+            print(f'{progress}/{total} {extension} failed to load')
+            print(f"→ {e.__class__.__name__}: {e}")
+    logger.info('\r… Modules loaded, logging in to Discord')
     
-@bot.slash_command(description="Returns info about the bot's ping")
-async def ping(ctx):
-    await ctx.respond(embed=simple_embed(f"⚡ {round(bot.latency * 1000)} ms"))
-
-module = SlashCommandGroup(
-    "module",
-    "Manage backend modules",
-)
-
-@module.command(description="Load a backend module", guild_ids=[SUPPORT_GUILD])
-@discord.ext.commands.is_owner()
-async def load(ctx, module: Option(str, "Name of the cog to load", autocomplete=discord.utils.basic_autocomplete(list_cogs()))):
-    try:
-        bot.load_extension(cogs_dir + "." + module)
-        await bot.register_commands()
-    except discord.ExtensionError as e:
-        await ctx.respond(embed=simple_embed("Load",'Failure',f'{e.__class__.__name__}: {e}'), ephemeral = True)
-        print(f"✗ {module} failed to load as requested by {ctx.author}")
-        print(f"→ {e.__class__.__name__}: {e}")
-    else:
-        await ctx.respond(embed=simple_embed("Load",'Success',f"{module} loaded"), ephemeral = True)
-        print(f"✓ {module} loaded by {ctx.author}")
-
-@module.command(description="Unload a backend module", guild_ids=[SUPPORT_GUILD])
-@discord.ext.commands.is_owner()
-async def unload(ctx, module: Option(str, "Name of the cog to unload", autocomplete=discord.utils.basic_autocomplete(list_cogs()))):
-    try:
-        bot.unload_extension(cogs_dir + "." + module)
-    except discord.ExtensionError as e:
-        await ctx.respond(embed=simple_embed("Unload",'Failure',f'{e.__class__.__name__}: {e}'), ephemeral = True)
-        print(f"✗ {module} failed to unload as requested by {ctx.author}")
-        print(f"→ {e.__class__.__name__}: {e}")
-    else:
-        await ctx.respond(embed=simple_embed("Unload",'Success',f"{module} unloaded"), ephemeral = True)
-        print(f"✓ {module} unloaded by {ctx.author}")
-        
-@module.command(description="Reload a backend module", guild_ids=[SUPPORT_GUILD])
-@discord.ext.commands.is_owner()
-async def reload(ctx, module: Option(str, "Name of the cog to reload", autocomplete=discord.utils.basic_autocomplete(list_cogs()))):
-    try:
-        bot.reload_extension(cogs_dir + "." + module) 
-        # slash cog reloading is, apparently, unsupported?
-        await bot.register_commands()
-    except discord.ExtensionError as e:
-        await ctx.respond(embed=simple_embed("Reload",'Failure',f'{e.__class__.__name__}: {e}'), ephemeral = True)
-        print(f"✗ {module} failed to reload as requested by {ctx.author}")
-        print(f"→ {e.__class__.__name__}: {e}")
-    else:
-        await ctx.respond(embed=simple_embed("Reload",'Success',f"{module} reloaded"), ephemeral = True)
-        print(f"✓ {module} reloaded by {ctx.author}")
         
 bot.run(TOKEN)
--- cogs/admin.py
+++ cogs/admin.py
@@ -1,10 +1,11 @@
 import logging
 import os
 
+from dotenv import load_dotenv
+
 import discord
 from discord.commands import Option, slash_command
 from discord.ext import commands
-from dotenv import load_dotenv
 
 from .utils.embed import simple_embed
 
@@ -12,6 +13,12 @@ load_dotenv()
 if os.getenv('SUPPORT_SERVER'): SUPPORT_SERVER = int(os.getenv('SUPPORT_SERVER'))
 
 logger = logging.getLogger('discord')
+
+def list_cogs(cogs_dir):
+    return [extension for extension in [f.replace('.py', '') for f in os.listdir(cogs_dir) if os.path.isfile(os.path.join(cogs_dir, f))]]
+    
+cogs_dir = "cogs"
+cogs = list_cogs(cogs_dir)
 
 class Admin(commands.Cog):
     def __init__(self, bot):
@@ -27,6 +34,42 @@ class Admin(commands.Cog):
     @slash_command(description="Returns info about the bot's ping")
     async def ping(self, ctx):
         await ctx.respond(embed=simple_embed(f"⚡ {round(self.bot.latency * 1000)} ms"))
+        
+    module = discord.SlashCommandGroup('module', description="Manage modules") 
+    
+    @module.command(description="Loads or reloads a backend module")
+    @discord.ext.commands.is_owner()
+    async def load(self, ctx, module: Option(str, "Name of the cog to load", autocomplete=discord.utils.basic_autocomplete(cogs))):
+        try:
+            if f"{cogs_dir}.{module}" in self.bot.extensions:
+                self.bot.reload_extension(cogs_dir + "." + module)
+                await self.bot.sync_commands()
+                await ctx.respond(embed=simple_embed("Load",'Success',f"🔃 {module} reloaded", ctx=ctx), ephemeral = True)
+                print(f"✓ {module} reloaded by {ctx.author}")
+            else:
+                self.bot.load_extension(cogs_dir + "." + module)
+                await self.bot.sync_commands()
+                await ctx.respond(embed=simple_embed("Load",'Success',f"📥 {module} loaded", ctx=ctx), ephemeral = True)
+                print(f"✓ {module} loaded by {ctx.author}")
+        except Exception as e:
+            await ctx.respond(embed=simple_embed("Load",'Failure',f'{e.__class__.__name__}: {e}', ctx=ctx), ephemeral = True)
+            print(f"✗ {module} failed to load as requested by {ctx.author}")
+            print(f"→ {e.__class__.__name__}: {e}")
+            
+    @module.command(description="Loads or reloads a backend module")
+    @discord.ext.commands.is_owner()
+    async def unload(self, ctx, module: Option(str, "Name of the cog to unload", autocomplete=discord.utils.basic_autocomplete(cogs))):
+        try:
+            self.bot.unload_extension(cogs_dir + "." + module)
+            await self.bot.sync_commands()
+            
+        except discord.ExtensionError as e:
+            await ctx.respond(embed=simple_embed("Unload",'Failure',f'{e.__class__.__name__}: {e}', ctx=ctx), ephemeral = True)
+            print(f"✗ {module} failed to unload as requested by {ctx.author}")
+            print(f"→ {e.__class__.__name__}: {e}")
+        else:
+            await ctx.respond(embed=simple_embed("Unload",'Success',f"{module} unloaded", ctx=ctx), ephemeral = True)
+            print(f"✓ {module} unloaded by {ctx.author}")
     
 
 def setup(bot):
