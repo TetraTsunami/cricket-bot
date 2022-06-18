9f7a11cd56e975f5b4d5b1b6c5a4cb3479ee3a6c
Gwen <akamitsunami@gmail.com>
2022-06-18 15:05:24 -0500

    Fixes for Minecraft Ping

--- cogs/Minecraft.py
+++ cogs/Minecraft.py
@@ -39,7 +39,7 @@ async def ping_server(server: str, plugins: bool, ctx: discord.ApplicationContex
     
     # Query the server if we're searching for plugins
     status_async = asyncio.create_task(target.async_status())
-    query_async = asyncio.create_task(target.async_query(tries=1)) if plugins else None
+    query_async = asyncio.create_task(target.async_query()) if plugins else None
     
     # Wait for the tasks to finish
     status = await status_async
@@ -53,33 +53,35 @@ async def ping_server(server: str, plugins: bool, ctx: discord.ApplicationContex
     embed.add_field(name="Ping", value=f"{round(status.latency)} ms")
     
     # Remove special characters from the description, and format newlines
-    description = re.sub(r'^\s+|§.', '', status.description)
+    description = re.sub(r'^\s+|§.', '', discord.utils.escape_markdown(status.description))
     description = re.sub(r'\n\s*', r'\n', description)
     embed.add_field(name="Description",
                     value=f"```{description}```", inline=False)
+    
+    # Hypixel, for instance, won't actually tell us their plugins/players even if we ask politely. They're sneaky like that, but Discord doesn't like an empty field.
+    if query and query.players.names:
+        # If we've already queried, may as well use the improved player list
+        embed.add_field(name="Online", value='\n'.join(discord.utils.escape_markdown(query.players.names)))
+    elif status.players.sample:
+        embed.add_field(name="Online", value='\n'.join(discord.utils.escape_markdown(player.name) for player in status.players.sample), inline=False)
 
-    # Let's try to get the server's icon
+    
+    if query:
+        if query.software.plugins:
+            embed.add_field(name="Plugins", value='\n'.join(query.software.plugins))
+            
+        # Let's try to get the server's icon
     try:
         data = base64.b64decode(
             status.favicon.split(',', 1)[-1])
         file = discord.File(fp=io.BytesIO(data),filename=f'{server}_icon.png')
         embed.set_thumbnail(url=f'attachment://{server}_icon.png')
     except:
-        pass
-    
-    # Hypixel, for instance, won't actually tell us their plugins/players even if we ask politely. They're sneaky like that, but Discord doesn't like an empty field.
-    if query and query.players.names:
-        # If we've already queried, may as well use the improved player list
-        embed.add_field(name="Online", value='\n'.join(query.players.names))
-    elif status.players.sample:
-        embed.add_field(name="Online", value='\n'.join(player.name for player in status.players.sample), inline=False)
-
-    
-    if query:
-        if query.software.plugins:
-            embed.add_field(name="Plugins", value='\n'.join(query.software.plugins))
-
+        file = discord.File(fp="./image_gen/default_server_icon.png", filename='default_icon.png')
+        embed.set_thumbnail(url=f'attachment://default_icon.png')
+            
     return embed, file
+    
             
 class MCRefreshButton(discord.ui.View):
     def __init__(self, server, plugins: bool = False, ctx: discord.ApplicationContext = None):
@@ -88,17 +90,17 @@ class MCRefreshButton(discord.ui.View):
         self.ctx = ctx
         self.plugins = plugins
 
-    @discord.ui.button(
-        style=discord.ButtonStyle.red, label="Refresh", custom_id="minecraft:refreshserver"
-    )
-    async def refresh(self, button: discord.ui.Button, interaction: discord.Interaction):
+    @discord.ui.button(label="Refresh", style=discord.ButtonStyle.red, custom_id="minecraft:refreshserver")
+    async def refreshButton (self, button: discord.ui.Button, interaction: discord.Interaction):
         try:
             await interaction.response.defer()
             embed, file = await ping_server(self.server, self.plugins, self.ctx)
             await interaction.message.edit(embed=embed, file=file, view=self)
         except Exception as e:
             await interaction.message.edit(embed=simple_embed(self.server, 'Minecraft',f'{e.__class__.__name__}: {e}'))
-        
+            
+            
+      
 class Minecraft(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
